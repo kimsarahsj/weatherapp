add43537f9bfb9d8462b1594684c55f2a3e7a018
kimsarahsj <53312812+kimsarahsj@users.noreply.github.com>
2023-02-01 17:40:41 -0500

    Add current project
    renamed and moved current weather project into weather app repo.

---
+++ src/weatherapp.js
@@ -0,0 +1,118 @@
+let now = new Date();
+//Start numerical date JS inject
+let todayDate = document.querySelector("#date");
+let date = now.getDate();
+todayDate.innerHTML = `${date}`;
+//End numerical date
+//Start actual day
+let todayDay = document.querySelector("#day");
+let days = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+let day = days[now.getDay()];
+todayDay.innerHTML = `${day}`;
+//End actual day
+
+//Start get month
+let todayMonth = document.querySelector("#month");
+let months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
+];
+let month = months[now.getMonth()];
+todayMonth.innerHTML = `${month}`;
+//End get month
+//Start get Hour
+let todayHour = document.querySelector("#hour");
+let hours = now.getHours();
+let ampm = "am";
+if (hours > 12) {
+  ampm = "pm";
+  hours = hours - 12;
+}
+todayHour.innerHTML = `${hours}`;
+// ampm logic saying if the value of hours is greater than 12 to subtract 12 and add pm
+//End get Hour
+//Start get Minutes
+let todayMinute = document.querySelector("#minute");
+let minutes = now.getMinutes();
+todayMinute.innerHTML = `${minutes} ${ampm}`;
+//appended ampm to minutes since not defined in html
+//End get Minutes
+
+//********HOMEWORK - SEARCH RESULT********
+//Start search bar city search & change location on card
+function updateForecast(response) {
+  let searchInput = document.querySelector("#search-city");
+  console.log(searchInput.value);
+  let cityLocation = document.querySelector("#location");
+  cityLocation.innerHTML = `${searchInput.value}`;
+  let temperature = Math.round(response.data.main.temp);
+  let todaysTemperature = document.querySelector("#temperature");
+  todaysTemperature.innerHTML = `${temperature}°`;
+  let condition = document.querySelector("#condition");
+  condition.innerHTML = response.data.weather[0].description;
+}
+
+function searchForecast() {
+  let city = document.querySelector("#search-city").value;
+  let encoded = encodeURI(city); //will send the city to the url even if a two word city is entered
+  let units = "imperial";
+  let key = "fe1483f743b581b5520a1b725af03a49";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encoded}&appid=${key}&units=${units}`;
+  axios.get(apiUrl).then(updateForecast);
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", searchForecast);
+//End search bar city search & change location on card
+
+//********BONUS - CURRENT LOCATION********
+//Start getting current location when current location button is clicked
+let lat;
+let long;
+function showPosition(position) {
+  lat = position.coords.latitude;
+  long = position.coords.longitude;
+  console.log(lat);
+  console.log(long);
+  let apiKey = "fe1483f743b581b5520a1b725af03a49";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(updatePage);
+}
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+//Start update the current temperature with current temp data when current location button is clicked.
+function updatePage(response) {
+  let temperature = Math.round(response.data.main.temp);
+  console.log(temperature);
+  console.log(response);
+  let todaysTemperature = document.querySelector("#temperature");
+  todaysTemperature.innerHTML = `${temperature}°`;
+  let latLongCity = response.data.name;
+  console.log(latLongCity);
+  let currentLocation = document.querySelector("#location");
+  currentLocation.innerHTML = `${latLongCity}`;
+}
+let currentCityBtn = document.querySelector("#locationBtn");
+currentCityBtn.addEventListener("click", getCurrentPosition);
+//End update the current temperature with current temp data when current location button is clicked.
